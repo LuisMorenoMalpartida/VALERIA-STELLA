f6ccff97d33ec28788fe5ea8e7b9c3a25d34339a
LuisMorenoMalpartida <gus2bmp@hotmail.com>
2024-10-01 12:20:48 -0500

    Hice el Navbar, si hay cambios lo hare

---
+++ src/INICIO/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Navbar.css';
+import { FaHeart, FaUserAlt, FaShoppingBag } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="top-banner">
+                ENCONTRARAS NUEVAS REFERENCIAS CADA SEMANA
+            </div>
+            <nav className="navbar">
+            <ul className="navbar-links">
+                <li>INICIO</li>
+                <li>MUJER</li>
+                <li>HOMBRE</li>
+                <li>OUTLET</li>
+            </ul>
+            <div className="navbar-logo">
+                VALERIA STELLA
+            </div>
+            <ul className="navbar-icons">
+                <li><FaUserAlt /></li>
+                <li><FaHeart /></li>
+                <li><FaShoppingBag /></li>
+            </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
