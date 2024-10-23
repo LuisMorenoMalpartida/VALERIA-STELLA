c5446a226de0205e2b9d32cf3ac5031046c31063
Laura Luque <lauraluque1215@hotmail.com>
2024-10-23 15:42:32 -0500

    style: :lipstick: SectionSwiper

--- src/features/Home/SectionSwiper/SectionSwiper.tsx
+++ src/features/Home/SectionSwiper/SectionSwiper.tsx
@@ -9,7 +9,6 @@ import "swiper/css";
 import "swiper/css/virtual";
 import "./SectionSwiper.css";
 import { Autoplay } from "swiper/modules";
-import { Button } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 
 export const SectionSwiper = () => {
@@ -32,7 +31,6 @@ export const SectionSwiper = () => {
         <SwiperSlide>
           <div className="slide-content">
             <img
-              // src="https://cc-prod.scene7.com/is/image/CCProdAuthor/Fashion-photography_P1_900x420?$pjpeg$&jpegSize=200&wid=900"
               src={ropa1}
               alt="Slide 1"
               className="slide-image"
@@ -40,34 +38,14 @@ export const SectionSwiper = () => {
             <div className="slide-text">
               <h2>COMPRA TUS PRENDAS FAVORITAS</h2>
               <div className="button-group">
-                <Button onClick={() => navigate("/mujer")}>VER MUJER</Button>
-                <Button onClick={() => navigate("/outlet")}>VER OUTLET</Button>
-                <Button onClick={() => navigate("/hombre")}>VER HOMBRE</Button>
-              </div>
+  <button className="custom-button" onClick={() => navigate("/mujer")}>VER MUJER</button>
+  <button className="custom-button" onClick={() => navigate("/outlet")}>VER OUTLET</button>
+  <button className="custom-button" onClick={() => navigate("/hombre")}>VER HOMBRE</button>
+</div>
+
             </div>
           </div>
         </SwiperSlide>
-        {/* <SwiperSlide>
-          <div className="slide-content">
-          <img src="https://www.ultraimagehub.com/wallpapers/tr:flp-false,gx-0.4,gy-0.5,q-75,rh-3264,rw-5824,th-1080,tw-1920/1240245968633466910.jpeg" 
-           alt="Slide 1" className="slide-image" />
-
-            <div className="slide-text">
-            <h2>COMPRA TUS PRENDAS FAVORITAS</h2>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="slide-content">
-          <img src="https://www.ultraimagehub.com/wallpapers/tr:flp-false,gx-0.4,gy-0.5,q-75,rh-3264,rw-5824,th-1080,tw-1920/1240245968633466910.jpeg" 
-
-          alt="Slide 1" className="slide-image" />
-
-            <div className="slide-text">
-            <h2>COMPRA TUS PRENDAS FAVORITAS</h2>
-            </div>
-          </div>
-        </SwiperSlide> */}
       </Swiper>
     </div>
   );
